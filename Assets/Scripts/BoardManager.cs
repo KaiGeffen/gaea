3a6132440c565298f2989ed0a5b49e7983aafa19
KaiGeffen <geffenkai@gmail.com>
2016-06-21 17:46:40 -0400

    1st Commit, floor generation works

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+
+	public GameObject[] floorTiles;
+	public GameObject[] playerTiles;
+	public GameObject[] enemyTiles;
+
+	private Transform boardHolder;
+	private List <Vector3> gridPositions = new List<Vector3>();
+
+	void InitialiseList ()
+	{
+		gridPositions.Clear ();
+		for (int x = 0; x < columns; x++) {
+			for (int y = 0; y < rows; y++) {
+				gridPositions.Add(new Vector3(x,y,0f));
+			}
+		}
+	}
+
+	void BoardSetup ()
+	{
+		boardHolder = new GameObject ("Board").transform;
+
+		for (int x = 0; x < columns; x++) {
+			for (int y = 0; y < rows; y++) {
+				GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length)];
+				GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent (boardHolder);
+			}
+		}
+	}
+
+	public void SetupScene () {
+		BoardSetup ();
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public BoardManager boardScript;
+
+	// Use this for initialization
+	void Start () {
+		boardScript.SetupScene ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
